a3e8e9861c707b826cb28bc478adc5defa8f59a1
Muzammil-hassan <muzammil.hassan1@techchefz.com>
2024-07-01 13:41:40 +0530

    feat: footer completed

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,19 +1,37 @@
-import { getMetadata } from '../../scripts/aem.js';
-import { loadFragment } from '../fragment/fragment.js';
+import { getMetadata } from "../../scripts/aem.js";
+import { loadFragment } from "../fragment/fragment.js";
 
 /**
  * loads and decorates the footer
  * @param {Element} block The footer block element
  */
 export default async function decorate(block) {
+  const footerEl = document.querySelector(".footer");
+
+  const descendants = footerEl.querySelectorAll("*");
+
+  // Loop through the descendants to find .default-content-wrapper
+  var contentWrapper = null;
+  descendants.forEach(function (element) {
+    console.log("element", element);
+    if (element.classList.contains("default-content-wrapper")) {
+      contentWrapper = element;
+      return; // Exit the loop once found
+    }
+  });
+
+  console.log("contentWrapper", contentWrapper);
+
   // load footer as fragment
-  const footerMeta = getMetadata('footer');
-  const footerPath = footerMeta ? new URL(footerMeta, window.location).pathname : '/footer';
+  const footerMeta = getMetadata("footer");
+  const footerPath = footerMeta
+    ? new URL(footerMeta, window.location).pathname
+    : "/footer";
   const fragment = await loadFragment(footerPath);
 
   // decorate footer DOM
-  block.textContent = '';
-  const footer = document.createElement('div');
+  block.textContent = "";
+  const footer = document.createElement("div");
   while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
 
   block.append(footer);
